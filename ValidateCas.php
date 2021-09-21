427b40bdcc404f00b3b5767e540501ecc93beb4d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-21 14:34:37 +0100

    Update ValidateCas.php

--- ValidateCas.php
+++ ValidateCas.php
@@ -16,6 +16,32 @@ class ValidateCas
         return str_word_count($nom)>1;
 
     }
-    
+    public function verifCode($code)
+    {
+        if(!$this->isNumber($code))
+        return false;
+        else
+        return strlen($code)===4;
+    }
+    function verifAdresse($adresse)
+    {
+        return strlen(trim($adresse));
+    }
+    function verifEtat($etat)
+    {
+        return $etat==1 || $etat==-1;
+    }
+    public function verifTel($tel)
+    {
+        if(strlen(trim($tel)<=8))
+        return false;
+        else
+            if(strpos('+',$tel)==0||strpos('00',$tel)==0)
+            return true;
+                else return false;
+
+
+
+    }
 }
 ?>
